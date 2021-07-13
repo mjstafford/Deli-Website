2f88c5ef213d88697184209cd6f6827fae3cfbaa
Michael <67665934+mjstafford@users.noreply.github.com>
2021-07-13 13:56:29 -0700

    updated tables so items can hold an image html and designate if the item is featured.

--- src/main/resources/db/migration/V2__create_items.sql
+++ src/main/resources/db/migration/V2__create_items.sql
@@ -3,5 +3,7 @@ CREATE TABLE items (
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   cost NUMERIC NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  featured BOOLEAN NOT NULL DEFAULT false,
   food_category_id INTEGER REFERENCES food_categories(id)
 );
