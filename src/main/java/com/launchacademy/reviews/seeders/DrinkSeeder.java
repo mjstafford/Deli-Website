25a3f9c2c10f770ca59c14093a249e642a55f374
Michael <67665934+mjstafford@users.noreply.github.com>
2021-07-13 13:41:53 -0700

    created tables, updated pom, added templates for future seeders

--- src/main/java/com/launchacademy/reviews/seeders/DrinkSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/DrinkSeeder.java
@@ -1,25 +1,25 @@
-package com.launchacademy.reviews.seeders;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class DrinkSeeder {
-  private DrinkService drinkService;
-
-  @Autowired
-  public DrinkSeeder(DrinkService drinkService) {
-    this.drinkService = drinkService;
-  }
-
-  public void seed(){
-    if(drinkService.count() == 0) {
-      Drink drink1 = new Drink();
-      drink1.setName("Coffee");
-      drink1.setDescription("Dark roasted");
-      drink1.setImgUrl("https://cdn-assets.alltrails.com/uploads/photo/image/30328785/extra_large_748839c73ca678cc22f24db1af0cebb1.jpg");
-
-      drinkService.save(drink1);
-    }
-  }
-}
+//package com.launchacademy.reviews.seeders;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class DrinkSeeder {
+//  private DrinkService drinkService;
+//
+//  @Autowired
+//  public DrinkSeeder(DrinkService drinkService) {
+//    this.drinkService = drinkService;
+//  }
+//
+//  public void seed(){
+//    if(drinkService.count() == 0) {
+//      Drink drink1 = new Drink();
+//      drink1.setName("Coffee");
+//      drink1.setDescription("Dark roasted");
+//      drink1.setImgUrl("https://cdn-assets.alltrails.com/uploads/photo/image/30328785/extra_large_748839c73ca678cc22f24db1af0cebb1.jpg");
+//
+//      drinkService.save(drink1);
+//    }
+//  }
+//}
--- src/main/java/com/launchacademy/reviews/seeders/MainSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/MainSeeder.java
@@ -1,26 +1,26 @@
-package com.launchacademy.reviews.seeders;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MainSeeder implements CommandLineRunner {
-  private DrinkSeeder drinkSeeder;
-  private MealSeeder mealSeeder;
-  private SideSeeder sideSeeder;
-
-  @Autowired
-  public MainSeeder(DrinkSeeder drinkSeeder, MealSeeder mealSeeder, SideSeeder sideSeeder) {
-    this.drinkSeeder = drinkSeeder;
-    this.mealSeeder = mealSeeder;
-    this.sideSeeder = sideSeeder;
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    drinksSeeder.seed();
-    mealSeeder.seed();
-    sideSeeder.seed();
-  }
-}
+//package com.launchacademy.reviews.seeders;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class MainSeeder implements CommandLineRunner {
+//  private DrinkSeeder drinkSeeder;
+//  private MealSeeder mealSeeder;
+//  private SideSeeder sideSeeder;
+//
+//  @Autowired
+//  public MainSeeder(DrinkSeeder drinkSeeder, MealSeeder mealSeeder, SideSeeder sideSeeder) {
+//    this.drinkSeeder = drinkSeeder;
+//    this.mealSeeder = mealSeeder;
+//    this.sideSeeder = sideSeeder;
+//  }
+//
+//  @Override
+//  public void run(String... args) throws Exception {
+//    drinksSeeder.seed();
+//    mealSeeder.seed();
+//    sideSeeder.seed();
+//  }
+//}
