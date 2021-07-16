45779412035a541094a2bbc66246187cd2555856
Michael <67665934+mjstafford@users.noreply.github.com>
2021-07-16 12:41:08 -0700

    updated styling

--- src/main/frontend/components/LandingPage.js
+++ src/main/frontend/components/LandingPage.js
@@ -60,7 +60,7 @@ const LandingPage = () => {
         <div className="about-us cell small-12 large-6">
             <h1 className="landing-text-header">About Us</h1>
             <hr className="landing-hr"/>
-            <img className="image" src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/34121324_103495260547748_6042988899226091520_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=6wu7V6SzaZEAX9Kladc&_nc_ht=scontent-sea1-1.xx&oh=42002d9f081de18e3925707c116a7740&oe=60F4F109" alt="chef Joe Shirley" width="300" height="200"  align="left"/>
+            <img className="image-chef" src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/34121324_103495260547748_6042988899226091520_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=6wu7V6SzaZEAX9Kladc&_nc_ht=scontent-sea1-1.xx&oh=42002d9f081de18e3925707c116a7740&oe=60F4F109" alt="chef Joe Shirley" width="300" height="200"  align="left"/>
             <> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur augue ac purus convallis, ultricies pellentesque nisi tristique. Ut ullamcorper nulla mauris, molestie iaculis sapien interdum vitae. Praesent in ex nisl. Etiam a finibus mauris. Phasellus rutrum sit amet mauris non elementum. Nunc in ligula a arcu auctor ultrices at eget elit. Pellentesque egestas ac velit at pellentesque. Mauris non consequat mauris. Donec vel odio mi. Aenean dictum, odio et cursus ornare, mauris lectus pretium risunsequat ultrices. Aliqua.</>
         </div>
 
