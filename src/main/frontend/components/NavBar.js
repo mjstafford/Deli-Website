42ccc7db8ba9dfe33298682fcc7bd988cd6d8c09
Michael <67665934+mjstafford@users.noreply.github.com>
2021-07-20 12:05:28 -0700

    removed foundation styling from navbar. Updated with standard css.

--- src/main/frontend/components/NavBar.js
+++ src/main/frontend/components/NavBar.js
@@ -1,54 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Redirect, Link } from 'react-router-dom'
 
 const NavBar = () => {
+  const [scrolling, setScrolling] = useState(false)
+
+  const smallNavbar = () => {
+    if(window.scrollY >= 100){
+      setScrolling(true)
+    } else {
+      setScrolling(false)
+    }
+  }
+
+  window.addEventListener('scroll', smallNavbar)
+
+  let classString = "top-bar NavBar sticky"
+  if(scrolling){
+    classString += ' active'
+  }
 
   return(
-    <div className="NavBar">
-      <div class="top-bar NavBar">
-        <div class="top-bar-left">
-          <ul class="dropdown menu" data-dropdown-menu>
-            <li>
-              <button>
-                <Link className="button-text" to="/">
-                  <span className="red"> Open Wide </span>
-                  <span className="green">Deli </span>
-                </Link>
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div class="top-bar-right">
-          <ul class="dropdown menu" data-dropdown-menu>
-            <li>
-              <a className=" NavBar button-text li-text" href="#">Menu</a>
-              <ul class="menu vertical">
-                <li>
-                  <Link className=" NavBar button-text li-text" to="/open-wide-deli/food">
-                    <span className="red"> Food </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className=" NavBar button-text li-text" to="/open-wide-deli/drinks">
-                    <span className="red"> Drinks </span>
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link className=" NavBar button-text li-text" to="/open-wide-deli/location">
-                <span className="red"> Location </span>
-              </Link>
-            </li>
-            <li>
-              <Link className=" NavBar button-text li-text" to="/open-wide-deli/about-us">
-                <span className="red"> About Us </span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="navbar sticky">
+      <div className="container-navbar-left">
+        <button>
+          <Link className="navbar-logo" to="/">Open Wide Deli</Link>
+        </button>
       </div>
-    </div>
+      <div className="container-navbar-right">
+        <div className="dropdown">
+          <button className="dropbtn">Menu Items ≡</button>
+          <div className="dropdown-content">
+            <Link className="" to="/open-wide-deli/food">Food</Link>
+            <Link className="" to="/open-wide-deli/drinks">Drinks</Link>
+          </div>
+        </div>
+          <Link className="" to="/open-wide-deli/location">Location</Link>
+          <Link className="" to="/open-wide-deli/about-us">About Us</Link>
+      </div>
+    </nav>
   )
 }
 
